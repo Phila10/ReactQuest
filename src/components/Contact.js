a5c409bf7ff1d079e43b51b40afdf7495f4b2059
Phila10 <aurelien.joubert01@gmail.com>
2020-03-30 18:13:47 +0200

    isuk

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,19 @@
 import React from 'react';
+import './Contact.css'
 
 function Contact() {
     const name = 'DJ KHALED';
     const online = true;
+    return (
     <div className="Contact">
-        <img src="https://randomuser.me/api/portraits/men/13.jpg"/>
-        <div>
-        <h4>{name}</h4>
-        <p>{online ? 'Online':'Offline'}</p>
+        <img className="avatar" src="https://randomuser.me/api/portraits/men/13.jpg"/>
+        <div>    
+        <h4 className="name">{name}</h4>
+        <div className="status">
+          <div className={online ? "status-online" : "status-offline"}></div>
+          <p className="status-text">{online ? "online" : "offline"}</p>
         </div>
-    </div>
+        </div>
+    </div>)
 }
-
-
-
-
-
-
-
 export default Contact;
