62699f6e58982583370d42b7bca5f8aa0ee12c3f
Phila10 <aurelien.joubert01@gmail.com>
2020-03-31 13:45:56 +0200

    MAP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Contact from './components/Contact'
+import ContactList from './components/ContactList'
 
 
 function App() {
   return (
     <div className="App">
-<Contact avatar="http://physiquedereve.fr/wp-content/uploads/2015/06/johnny-depp-visage-portrait-beau-gosse.jpg" name="AUXENCE TROP BO" online/>
-<Contact avatar="https://randomuser.me/api/portraits/women/42.jpg" name="DJ KHALED"/>
-<Contact avatar="https://www.chine-nouvelle.com/jdd/public/documents/cc/olive/20100615-patapouf.1.jpg" name="Kim Jong-un" online/>
+  <ContactList />
     </div>
   )
 }
---
+++ src/components/ContactList.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Contact from './Contact'
+
+
+const users = [
+    {
+      name: 'AUXENCE WAOUW',
+      avatar: 'http://physiquedereve.fr/wp-content/uploads/2015/06/johnny-depp-visage-portrait-beau-gosse.jpg',
+      online: false
+    },
+    {
+      name: 'DJ KHAAAALED',
+      avatar: 'https://randomuser.me/api/portraits/women/42.jpg',
+      online: true
+    },
+    {
+      name: 'Kim jong-un',
+      avatar: 'https://www.chine-nouvelle.com/jdd/public/documents/cc/olive/20100615-patapouf.1.jpg',
+      online: true
+    },
+    {
+      name: 'Borock abomo',
+      avatar: 'https://scantlessscank.files.wordpress.com/2012/03/funny-barack-obama-white-man-1.jpg?w=640',
+      online: false
+    },
+    {
+      name: 'pas idée',
+      avatar: 'https://risibank.fr/cache/stickers/d1206/120664-full.png',
+      online: true
+    }
+  ];
+  const ContactList = () => (
+      <div> {users.map(ok => (<Contact name={ok.name} avatar={ok.avatar} online={ok.online} key={ok.name}/>))}</div>
+  )
+
+export default ContactList;
